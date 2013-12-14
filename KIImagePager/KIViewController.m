9b6c2f2c60f6cce0986f4d856092c439e8ee5e4d
Marcus Kida <marcus@kida.io>
2013-12-14 12:52:52 +0100

    Add whitespace for captionForImageAtIndex delegate method in sample

--- KIImagePager/KIViewController.m
+++ KIImagePager/KIViewController.m
@@ -53,7 +53,7 @@
     return UIViewContentModeScaleAspectFill;
 }
 
-- (NSString *)captionForImageAtIndex:(NSUInteger)index
+- (NSString *) captionForImageAtIndex:(NSUInteger)index
 {
     return @[
              @"First screenshot",
